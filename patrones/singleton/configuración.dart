f9614e5f8a71c24e29c0d6f2ed8607f4f39f3bdf
dev-brian <dev.brianoctavio@gmail.com>
2026-09-24 14:48:20 -0600

    Implement singleton pattern in Configuracion

--- patrones/singleton/configuración.dart
+++ patrones/singleton/configuración.dart
@@ -1,5 +1,12 @@
 class Configuracion {
-  String? idioma;
+  //Es un constructor privado para que no se pueda crear una instancia de la clase fuera de la clase
+  Configuracion._();
 
-  Configuracion(this.idioma);
+  //Es una instancia estática de la clase lo que es static: vive durante el ciclo de la app
+  static final Configuracion _instancia = Configuracion._();
+
+  //Es un factory que retorna la instancia estática
+  factory Configuracion() => _instancia;
+
+  String idioma = 'es';
 }
--- patrones/singleton/main.dart
+++ patrones/singleton/main.dart
@@ -1,10 +1,18 @@
 import 'configuración.dart';
 
 void main() {
-  final pantallaInicio = Configuracion('es');
-  final pantallaPerfil = Configuracion('es');
+  final pantallaInicio = Configuracion();
+  final pantallaPerfil = Configuracion();
+
+  print('Inicio de la pantalla de inicio:${pantallaInicio.idioma}');
+  print('Inicio de la pantalla de perfil:${pantallaPerfil.idioma}');
   pantallaInicio.idioma = 'en';
 
   print('Inicio de la pantalla de inicio:${pantallaInicio.idioma}');
   print('Inicio de la pantalla de perfil:${pantallaPerfil.idioma}');
+  print(
+    identical(pantallaInicio, pantallaPerfil)
+        ? 'Ambas pantallas son la misma instancia.'
+        : 'Las pantallas son instancias diferentes',
+  );
 }
